364e45814b7055d0e6cda811b1065a791cbb58a4
cospin89 <cospin9761@gmail.com>
2025-02-17 23:07:04 +0100

    supabase database support added

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -11,11 +11,3 @@ def init_db(app):
 
     with app.app_context():
         db.create_all()
-
-        test_user = User(
-            email="user@user.com",
-            password_hash=generate_password_hash("user")  # Hash the password
-        )
-        db.session.add(test_user)
-        db.session.commit()
-        print("Test user created: user@user.com / user")
